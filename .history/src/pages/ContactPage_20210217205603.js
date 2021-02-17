569043d87c039ae84fe934d685d0dd3c981a4c88
Krzysztofa Kołodziejczak <krzysztofa.kolodziejczak@gmail.com>
2021-02-17 20:57:37 +0100

    bbb

---
+++ .history/src/pages/ContactPage_20210217205603.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container, TitlePage } from './styled/styled';
+const ContactPage = () => {
+  return (
+    <Container>
+      <TitlePage>
+        Contact Page dnjhdwjhd
+      </TitlePage>
+    </Container>
+  )
+}
+
+export default ContactPage;
